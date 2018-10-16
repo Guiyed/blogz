75bffbf0c2b6a148d485cfa320f973df71542c96
Guillermo Zerpa <guillermo.zerpa@hotmail.com>
2018-10-16 16:52:11 -0400

    All templates implemented (Assigment: 80% completion)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
 import validators
@@ -6,9 +6,10 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:lc101@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:lc101@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'GSeVB/.xkffavmur; jvst.lali9jqA'
 
 
 class Blog(db.Model):
@@ -18,23 +19,50 @@ class Blog(db.Model):
     body = db.Column(db.String(500))
     pub_date = db.Column(db.DateTime)
     deleted = db.Column(db.Boolean)
-
-    def __init__(self, title, body, pub_date=None):
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    
+    def __init__(self, owner, title, body, pub_date=None):
+        self.owner = owner
         self.title = title
         self.body = body
         if pub_date is None:
             pub_date = datetime.utcnow()
         self.pub_date = pub_date
         self.deleted = False
+        
 
     def __repr__(self):
         return '<Blog %r>' % self.title
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(32))
+    email = db.Column(db.String(120),unique=True)
+    blogs = db.relationship('Blog', backref='owner') 
+
+
+    def __init__(self, username, password, email=None):
+        self.username = username
+        self.password = password
+        if email is None:
+            email = ''
+        self.email = email
+
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return redirect('/blog')
+    #users = Blog.query.order_by(User.username).all()
+    users = User.query.order_by(User.username).all()
+    return render_template('index.html',title="Blogz | Home ", users=users)  
 
 
 
@@ -48,10 +76,10 @@ def blog():
 
     if blog_id:
         indv_blog = Blog.query.get(blog_id)
-        return render_template('individualblog.html',title="Build a Blog!", blogs=blogs, blog=indv_blog)
+        return render_template('individualblog.html',title="Blogz | Post", blogs=blogs, blog=indv_blog)
     else:
         #return render_template('blog.html',title="Build a Blog!", blogs=blogs, deleted_blogs=deleted_blogs)
-        return render_template('blog.html',title="Build a Blog!", blogs=blogs_ordered_most_recent, deleted_blogs=deleted_blogs)
+        return render_template('blog.html',title="Blogz | Posts", blogs=blogs_ordered_most_recent, deleted_blogs=deleted_blogs)
 
 
 
@@ -67,17 +95,20 @@ def newpost():
         bodyError = validators.validateBody(blog_body)
 
         if not titleError and not bodyError:
-            new_blog = Blog(blog_title,blog_body)
+            #add owner to blog
+            owner = User.query.filter_by(username=session['user']).first()    
+
+            new_blog = Blog(owner, blog_title,blog_body)
             db.session.add(new_blog)
             db.session.commit()
             #return redirect('/blog')
             return redirect('./blog?id={0}'.format(new_blog.id))
 
         else:
-            return render_template('newpost.html',title="Build a Blog! | New entry", blogTitle= blog_title, blogBody= blog_body, titleError= titleError, bodyError= bodyError)
+            return render_template('newpost.html',title="Blogz | New post", blogTitle= blog_title, blogBody= blog_body, titleError= titleError, bodyError= bodyError)
 
     else:
-        return render_template('newpost.html',title="Build a Blog! | New entry")
+        return render_template('newpost.html',title="Blogz | New post")
      
      
 
@@ -91,6 +122,82 @@ def delete_entry():
     #db.session.commit()
 
     return redirect('/')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+
+    if request.method == 'POST': 
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            session['user'] = user.username
+            flash("Logged in",'success')
+            return redirect('/newpost')
+        elif not user:
+            flash('User does not exist', 'warning')
+            return redirect('/login')
+        else:
+            flash('User / password combination is incorrect', 'warning')
+            return redirect('/login')
+    return render_template('login.html',title='Blogz | Login')
+
+
+@app.route('/signup', methods = ['POST', 'GET'])
+def signup():
+
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        retypepass = request.form['retypepass']
+
+        # Validate user's data
+        usernameError = validators.validateUsername(username)
+        passwordError = validators.validatePassword(password)
+        retypeError = validators.validateRetype(password,retypepass)
+        emailError = validators.validateEmail(email)
+
+
+        if not usernameError and not passwordError and not retypeError and not emailError:
+            
+            existing_user = User.query.filter_by(username=username).first()
+
+            if not existing_user:
+                new_user = User(username,password,email)
+                db.session.add(new_user)
+                db.session.commit()
+                # "Remember" the user
+                session['user'] = new_user.username
+                flash("Signed in",'success')
+                return redirect('/newpost')
+            else:               
+                flash("Duplicated User, please try again",'error')
+                return redirect('/signup')
+
+        else:
+            flash("One or more Fields are invalid, please try again",'warning')
+            return render_template('signup.html',title='Blogz | Signup', username= username, email= email, usernameError= usernameError, passwordError= passwordError, retypeError= retypeError, emailError= emailError)
+        
+    return render_template('signup.html', title='Blogz | Signup')
+
+
+@app.route('/logout')
+def logout():
+    if session:
+        del session['user']
+        flash("Logged Out",'success')
+    return redirect('/blog')
+
+
+@app.before_request
+def require_login():
+  allowed_routes = ['login','blog', 'index', 'signup','static']
+  if request.endpoint not in allowed_routes and 'user' not in session:
+    return redirect('/login')
+
 
 
 if __name__ == '__main__':
--- validators.py
+++ validators.py
@@ -4,25 +4,66 @@ def validateTitle(blogTitle):
   if blogTitle == "":
     return 'Please fill in the Title'
   elif not re.match("^[A-Za-z0-9\s\-_,\.!:;()''""]{1,100}$", blogTitle):
-    return 'Title must contain between 1 and 100 alphanumeric characters'  
-  
+    return 'Title must contain between 1 and 100 alphanumeric characters' 
   return ''
 
 
 def validateBody(bodyText):
   if bodyText == "":
     return "Please fill in the Body of the Blog"
-
   elif not re.match("^[A-Za-z0-9\s\-_,\!.:;()''""]{1,500}$",bodyText):
     return 'Password must be between 1 and 500 characters'    
   return ''
 
 
+def validateUsername(username):
+  if username == "":
+    return 'User is empty'
+  elif not re.match("^[A-Za-z]{3,20}$", username):
+    return 'User must contains between 3 and 20 alphanumeric characters'  
+  return ''
+
+
+def validatePassword(password):
+  if password == "":
+    return "Password is empty"
+  elif not re.match("^[A-Za-z0-9]{3,20}$", password):
+    return 'Password must be between 3 and 20 characters'
+  return ''
+
+def validateRetype(element,retype):
+  if len(element) > 0:
+    if element != retype:
+      return "Passwords do not match"      
+  return ''
+
+
+def validateEmail(email):
+  if email:
+    if not re.match("^[A-Za-z0-9]{3,20}@[A-Za-z]+\.[a-zA-Z]+$", email):
+      return "Email must be between 3 and 20 characters, must contains one '@', one '.' and no empty '  ' spaces "  
+  return ''
+
+      #^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$"
+      #elif not re.match("[^@]+@[^@]+\.[^@]+", email):
+
+
+
+
 def main():
   title = 'gui que mas'
   body = '1234 kdsjfs f-fe felkj '
+  user = 'gui'
+  passw = '1234'
+  mail = 'hd3@q.c'
+  
   print(validateTitle(title))  
   print(validateBody(body))
+  print(validateUsername(user))  
+  print(validatePassword(passw))   
+  print(validateRetype(passw,'1234')) 
+  print(validateEmail(mail))
+
 
   title = ''
   body = ''
